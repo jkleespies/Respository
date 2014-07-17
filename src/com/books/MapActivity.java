a9ca1dda0add28bb308b66bf8e52e20e6bf02efe
Janette <janette.kleespies@gmx.de>
2014-07-17 23:14:07 +0200

    libs import camera didn´t work

--- src/com/books/MapActivity.java
+++ src/com/books/MapActivity.java
@@ -1,18 +1,9 @@
 // create map add marker with current position and show nearby places
 package com.books;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -38,275 +29,243 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-
 public class MapActivity extends Activity implements LocationListener {
 
-	//instance variables for Marker icon drawable resources
+	// instance variables
 	private int userIcon, shopIcon;
-
-	//the map
 	private GoogleMap theMap;
-
-	//location manager
 	private LocationManager locMan;
-
-	//user marker
 	private Marker userMarker;
-	
-	//places of interest
 	private Marker[] placeMarkers;
-	//max
-	private final int MAX_PLACES = 20;//most returned from google
-	//marker options
+	private final int MAX_PLACES = 20;// number of places returned by API
 	private MarkerOptions[] places;
 	protected MapFragment mapFragment;
-	// searchphrase
-	String types = "book_store|library";
-	
+	private String placesSearchStr;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-
-		//get drawable IDs
+		// get icons
 		userIcon = R.drawable.location;
 		shopIcon = R.drawable.location_place;
 
-		mapFragment = MapFragment.newInstance();
+		this.mapFragment = MapFragment.newInstance();
 		getFragmentManager().beginTransaction()
-			.add(R.id.the_map, mapFragment)
-			.commit();
-		
-	
-		try {
-		types = URLEncoder.encode(types, "UTF-8");
-		} catch (UnsupportedEncodingException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-		}
+				.add(R.id.the_map, this.mapFragment).commit();
 	}
-	
-	protected void onStart(){
-		super.onStart();
-		
-		//find out if we already have it
-				if(theMap==null){
-					theMap = mapFragment.getMap();
-					// set MapType, place markers
-						theMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-						placeMarkers = new Marker[MAX_PLACES];
-						// call method updatePlaces 
-						updatePlaces();
 
-				}
+	protected void onStart() {
+		super.onStart();
+		// check if map allready exists
+		// if(theMap==null){
+		// get the map
+		// theMap =
+		// ((MapFragment)getFragmentManager().findFragmentById(R.id.the_map)).getMap();
+		this.theMap = this.mapFragment.getMap();
+		// check in case map/ Google Play services not available
+		if (this.theMap != null) {
+			// ok - proceed
+			this.theMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+			// create marker array
+			placeMarkers = new Marker[MAX_PLACES];
+			// update location
+			updatePlaces();
+		}
+		// }
 	}
-	
-	//location listener functions
-	
+
 	@Override
 	public void onLocationChanged(Location location) {
-		Log.v("MyMapActivity", "location changed");
 		updatePlaces();
 	}
+
 	@Override
-	public void onProviderDisabled(String provider){
-		Log.v("MyMapActivity", "provider disabled");
+	public void onProviderDisabled(String provider) {
+
 	}
+
 	@Override
 	public void onProviderEnabled(String provider) {
-		Log.v("MyMapActivity", "provider enabled");
+
 	}
+
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
-		Log.v("MyMapActivity", "status changed");
+
 	}
-	
-	/*
-	 * update the place markers
-	 */
-	private void updatePlaces(){
-		//get location manager
-		locMan = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		//get last location
-		Location lastLoc = locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+	// update the markers
+	private void updatePlaces() {
+		// get location manager
+		locMan = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		// get last location
+		Location lastLoc = locMan
+				.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 		double lat = lastLoc.getLatitude();
 		double lng = lastLoc.getLongitude();
-		//create LatLng
+		// create LatLng
 		LatLng lastLatLng = new LatLng(lat, lng);
 
-		//remove any existing marker
-		if(userMarker!=null) userMarker.remove();
-		//create and set marker properties
-		userMarker = theMap.addMarker(new MarkerOptions()
-		.position(lastLatLng)
-		.title("You are here")
-		.icon(BitmapDescriptorFactory.fromResource(userIcon)));
-		//move to location
+		// remove existing marker
+		if (userMarker != null)
+			userMarker.remove();
+		// create and set the marker properties
+		userMarker = this.theMap.addMarker(new MarkerOptions()
+				.position(lastLatLng).title("You are here")
+				.icon(BitmapDescriptorFactory.fromResource(userIcon)));
+
+		// set camera position
 		CameraPosition cameraPosition = new CameraPosition.Builder()
-			.target(lastLatLng)
-			.zoom(15)
-			.tilt(45).build();
-			
-		theMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-		
-		String placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/" +
-		"json?location="+lat+","+lng+
-		"&radius=1000&sensor=true" +
-		"&types=" + types +
-		"&key=AIzaSyBt4UwId11OPjLA5jjnNuTAvJ-d-1FbAFM";
-		        
-		
-		//execute query
-		new GetPlaces().execute(placesSearchStr);
-		
-		locMan.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 30000, 100, this);
+				.target(lastLatLng).zoom(15).tilt(45).build();
+		this.theMap.moveCamera(CameraUpdateFactory
+				.newCameraPosition(cameraPosition));
+
+		// define search parameters for places -> type
+		String types = "book_store|library";
+		try {
+			types = URLEncoder.encode(types, "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+		// create API search string
+		placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/"
+				+ "json?location="
+				+ lat
+				+ ","
+				+ lng
+				+ "&radius=1000&sensor=true"
+				+ "&types="
+				+ types
+				+ "&key=AIzaSyBt4UwId11OPjLA5jjnNuTAvJ-d-1FbAFM";
+
+		// run query
+		new GetPlaces().execute();
+
+		// set Update settings (choosen Provider, min time interval between
+		// updates, min meters before update, the listener)
+		locMan.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 30000,
+				100, this);
 	}
-	
+
+	// get the places from Google API as JSON and parse
 	private class GetPlaces extends AsyncTask<String, Void, String> {
 		@Override
-		protected String doInBackground(String... placesURL) {
-			//fetch places
-			
-			//build result as string
-			StringBuilder placesBuilder = new StringBuilder();
-			//process search parameter string(s)
-			for (String placeSearchURL : placesURL) {
-				HttpClient placesClient = new DefaultHttpClient();
-				try {
-					//try to fetch the data
-					
-					//HTTP Get receives URL string
-					HttpGet placesGet = new HttpGet(placeSearchURL);
-					//execute GET with Client - return response
-					HttpResponse placesResponse = placesClient.execute(placesGet);
-					//check response status
-					StatusLine placeSearchStatus = placesResponse.getStatusLine();
-					//only carry on if response is OK
-					if (placeSearchStatus.getStatusCode() == 200) {
-						//get response entity
-						HttpEntity placesEntity = placesResponse.getEntity();
-						//get input stream setup
-						InputStream placesContent = placesEntity.getContent();
-						//create reader
-						InputStreamReader placesInput = new InputStreamReader(placesContent);
-						//use buffered reader to process
-						BufferedReader placesReader = new BufferedReader(placesInput);
-						//read a line at a time, append to string builder
-						String lineIn;
-						while ((lineIn = placesReader.readLine()) != null) {
-							placesBuilder.append(lineIn);
-						}
-					}
-				}
-				catch(Exception e){ 
-					e.printStackTrace(); 
-				}
-			}
-			return placesBuilder.toString();
+		protected String doInBackground(String... args0) {
+
+			String url = placesSearchStr.replaceAll("[ ]", "%20");
+			Adapter ad = new Adapter();
+
+			// URL request and response from Adapter
+			String jsonStr = ad.makeServiceCall(url, Adapter.GET);
+
+			return jsonStr;
+
 		}
-		//process data retrieved from doInBackground
+
+		// process data retrieved from doInBackground
 		protected void onPostExecute(String result) {
-			//parse place data returned from Google Places
-			//remove existing markers
-			if(placeMarkers!=null){
-				for(int pm=0; pm<placeMarkers.length; pm++){
-					if(placeMarkers[pm]!=null)
+			// parse place data returned from Google Places API
+			// remove existing markers
+			if (placeMarkers != null) {
+				for (int pm = 0; pm < placeMarkers.length; pm++) {
+					if (placeMarkers[pm] != null)
 						placeMarkers[pm].remove();
 				}
 			}
+			// parse JSON
 			try {
-				//parse JSON
-				
-				//create JSONObject, pass stinrg returned from doInBackground
+
+				// create JSONObject, pass sting returned from doInBackground
 				JSONObject resultObject = new JSONObject(result);
-				//get "results" array
+				// get "results" array
 				JSONArray placesArray = resultObject.getJSONArray("results");
-				//marker options for each place returned
+				// marker options for each place returned
 				places = new MarkerOptions[placesArray.length()];
-				//loop through places
-				for (int p=0; p<placesArray.length(); p++) {
-					//parse each place
-					//if any values are missing we won't show the marker
-					boolean missingValue=false;
-					LatLng placeLL=null;
-					String placeName="";
-					String vicinity="";
+				// loop through places
+				for (int p = 0; p < placesArray.length(); p++) {
+					// parse each place
+					// if any values are missing we won't show the marker
+					boolean missingValue = false;
+					LatLng placeLL = null;
+					String placeName = "";
+					String vicinity = "";
 					int currIcon = shopIcon;
-					try{
-						//attempt to retrieve place data values
-						missingValue=false;
-						//get place at this index
+					try {
+						// attempt to retrieve place data values
+						missingValue = false;
+						// get place at this index
 						JSONObject placeObject = placesArray.getJSONObject(p);
-						//get location section
+						// get location section
 						JSONObject loc = placeObject.getJSONObject("geometry")
 								.getJSONObject("location");
-						//read lat lng
-						placeLL = new LatLng(Double.valueOf(loc.getString("lat")), 
-								Double.valueOf(loc.getString("lng")));	
-						//get types
+						// read lat lng
+						placeLL = new LatLng(Double.valueOf(loc
+								.getString("lat")), Double.valueOf(loc
+								.getString("lng")));
+						// get types
 						JSONArray types = placeObject.getJSONArray("types");
-						//loop through types
-						for(int t=0; t<types.length(); t++){
-							//what type is it
-							String thisType=types.get(t).toString();
-							//check for particular types - set icons
-							if(thisType.contains("book_store")){
+						// loop through types
+						for (int t = 0; t < types.length(); t++) {
+							// what type is it
+							String thisType = types.get(t).toString();
+							// check for particular types - set icons
+							if (thisType.contains("book_store")) {
+								currIcon = shopIcon;
+								break;
+							} else if (thisType.contains("library")) {
 								currIcon = shopIcon;
 								break;
 							}
-							else if(thisType.contains("library")){
-								currIcon = shopIcon;
-								break;
-							}
-							
+
 						}
-						//vicinity
+						// vicinity
 						vicinity = placeObject.getString("vicinity");
-						//name
+						// name
 						placeName = placeObject.getString("name");
-					}
-					catch(JSONException jse){
+					} catch (JSONException jse) {
 						Log.v("PLACES", "missing value");
-						missingValue=true;
+						missingValue = true;
 						jse.printStackTrace();
 					}
-					//if values missing we don't display
-					if(missingValue)	places[p]=null;
+					// if values missing we don't display
+					if (missingValue)
+						places[p] = null;
 					else
-						places[p]=new MarkerOptions()
-					.position(placeLL)
-					.title(placeName)
-					.icon(BitmapDescriptorFactory.fromResource(currIcon))
-					.snippet(vicinity);
+						places[p] = new MarkerOptions()
+								.position(placeLL)
+								.title(placeName)
+								.icon(BitmapDescriptorFactory
+										.fromResource(currIcon))
+								.snippet(vicinity);
 				}
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			if(places!=null && placeMarkers!=null){
-				for(int p=0; p<places.length && p<placeMarkers.length; p++){
-					//will be null if a value was missing
-					if(places[p]!=null)
-						placeMarkers[p]=theMap.addMarker(places[p]);
+			if (places != null && placeMarkers != null) {
+				for (int p = 0; p < places.length && p < placeMarkers.length; p++) {
+					// will be null if a value was missing
+					if (places[p] != null)
+						placeMarkers[p] = theMap.addMarker(places[p]);
 				}
 			}
-			
+
 		}
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		if(theMap!=null){
-			locMan.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 30000, 100, this);
+		if (this.theMap != null) {
+			locMan.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+					30000, 100, this);
 		}
 	}
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
-		if(theMap!=null){
+		if (this.theMap != null) {
 			locMan.removeUpdates(this);
 		}
 	}
@@ -321,10 +280,10 @@ public class MapActivity extends Activity implements LocationListener {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// initialize Intent 
+		// initialize Intent
 		Intent i;
-		// Switch case between buttons, start activities 
-		switch (item.getItemId()){
+		// Switch case between buttons, start activities
+		switch (item.getItemId()) {
 		case R.id.search:
 			i = new Intent(getApplicationContext(), SearchActivity.class);
 			startActivity(i);
@@ -340,7 +299,7 @@ public class MapActivity extends Activity implements LocationListener {
 		default:
 			Log.d("onOptionsItemSelected", "es wurde nichts aufgewählt");
 		}
-			return super.onOptionsItemSelected(item);
-		
+		return super.onOptionsItemSelected(item);
+
 	}
 }
--- src/com/books/SearchActivity.java
+++ src/com/books/SearchActivity.java
@@ -5,58 +5,82 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SearchActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_search);
-		
-		// call scanner 
-		Button scannerButton = (Button)findViewById( R.id.searchActivity_scanner );
-		scannerButton.setOnClickListener( new View.OnClickListener(){
-			
-			@Override
-        	public void onClick(View v){
-				Intent i = new Intent(getApplicationContext(), ScannerActivity.class);
-				startActivityForResult(i, 0);
-			}
-		});
-		
-		Button searchButton = (Button)findViewById( R.id.searchActivity_search );
-		searchButton.setOnClickListener( new View.OnClickListener(){
-			
-			@Override
-        	public void onClick(View v){
-				EditText search_keyword = (EditText)findViewById(R.id.searchActivity_input_field);
-				String search_phrase = search_keyword.getText().toString();
-				
-				Intent i = new Intent(getApplicationContext(), SearchResultActivity.class);
-				i.putExtra("searchfor", search_phrase);
-				startActivity(i);
-			}
-		});
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_search);
 
+	Button scannerButton = (Button) findViewById(R.id.searchActivity_scanner);
+	scannerButton.setOnClickListener(new View.OnClickListener() {
+
+	@Override
+	public void onClick(View v) {
+		
+	Intent intent = new Intent(getApplicationContext(),	ZBarScannerActivity.class);
+	startActivityForResult(intent, 0);
+	}
+	});
+
+	Button searchButton = (Button) findViewById(R.id.searchActivity_search);
+	searchButton.setOnClickListener(new View.OnClickListener() {
+
+	@Override
+	public void onClick(View v) {
+	EditText search_keyword = (EditText) findViewById(R.id.searchActivity_input_field);
+	String search_phrase = search_keyword.getText().toString();
+	if (search_phrase.compareTo("") == 0) {
+	Toast toast = Toast.makeText(getApplicationContext(),
+	"wronge input", Toast.LENGTH_SHORT);
+	toast.setGravity(Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
+	toast.show();
+	} else {
+	Intent i = new Intent(getApplicationContext(),
+	SearchResultActivity.class);
+	i.putExtra("searchfor", search_phrase);
+	startActivity(i);
+	}
 
 	}
+	});
+
 	
-	// get result from scanner
-	 @Override
-	    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-	    	super.onActivityResult(requestCode, resultCode, data);
-	    	
-			String returnValue = data.getExtras().getString("scanresult");
-			//start Activity Result
-	    	Intent i = new Intent(getApplicationContext(), SearchResultActivity.class);
-	    	i.putExtra("searchfor", returnValue);
-	    	startActivity(i);
-	    }
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+	if (resultCode == RESULT_OK) {
+	// Scan result is available by making a call to
+	// data.getStringExtra(ZBarConstants.SCAN_RESULT)
+	// Type of the scan result is available by making a call to
+	// data.getStringExtra(ZBarConstants.SCAN_RESULT_TYPE)
+	Toast.makeText(	this,"Scan Result = "+ data.getStringExtra(ZBarConstants.SCAN_RESULT),Toast.LENGTH_SHORT).show();
+	String returnValue = data.getStringExtra(ZBarConstants.SCAN_RESULT);
+
+	Intent i = new Intent(getApplicationContext(),
+	SearchResultActivity.class);
+	i.putExtra("searchfor", returnValue);
+
+	startActivity(i);
+
+	// The value of type indicates one of the symbols listed in Advanced
+	// Options below.
+	} else if (resultCode == RESULT_CANCELED) {
+	Toast.makeText(this, "Camera unavailable", Toast.LENGTH_SHORT)
+	.show();
+	}
+
+	}
 
 		@Override
 		public boolean onCreateOptionsMenu(Menu menu) {
@@ -88,7 +112,5 @@ public class SearchActivity extends Activity {
 				Log.d("onOptionsItemSelected", "es wurde nichts aufgewählt");
 			}
 				return super.onOptionsItemSelected(item);
-			
 		}
-	 
 }
